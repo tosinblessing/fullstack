514585597f6bd6cbae01f3e0f71a2cb7b76b567b
tosinblessing <tosinblessing.shittu@gmail.com>
2020-08-05 10:59:14 +0100

    generate question model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions(){
+        $this->hasMany(Question::class);
+    }
+    // doing this because of slug
+    public function setTitleAttributes($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
