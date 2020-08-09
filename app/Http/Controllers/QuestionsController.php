e51965052d028c9a55366735d2c94af16cf2e090
tosinblessing <tosinblessing.shittu@gmail.com>
2020-08-09 16:52:03 +0100

    using policy to limit unauhtorize edit and delete

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -10,6 +10,11 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -78,6 +83,7 @@ class QuestionsController extends Controller
     public function edit(Question $question)
     {
         //
+        $this->authorize('update', $question);
        return view('questions.edit', compact('question'));
     }
 
@@ -91,6 +97,7 @@ class QuestionsController extends Controller
     public function update(AskQuestionRequest $request, Question $question)
     {
         //
+        $this->authorize('update', $question);
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', 'Your question has been updated');
     }
@@ -104,6 +111,7 @@ class QuestionsController extends Controller
     public function destroy(Question $question)
     {
         //
+        $this->authorize('delete', $question);
         $question->delete();
         return redirect('/questions')->with('success', 'Your question has been deleted');
     }
